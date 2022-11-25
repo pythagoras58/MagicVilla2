2fd648fd18efb2b4491e7f279774e26ae15a176a
SolomonDa <solomonda@stlghana.com>
2022-11-25 10:51:23 +0000

    Add project files.

---
+++ MagicVillaAPI2/Controllers/MagicVillaController.cs
@@ -0,0 +1,19 @@
+﻿using MagicVillaAPI2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVillaAPI2.Controllers
+{
+    [Route("api/MagicVillaController")]
+    [ApiController]
+    public class MagicVillaController : ControllerBase
+    {
+        public IEnumerable<VillaModel> GetVillaModels()
+        {
+            return new List<VillaModel>
+            {
+                new VillaModel {Id=1, Name="Pool View"},
+                new VillaModel {Id=2,Name="Beach View"}
+            };
+        }
+    }
+}
---
+++ MagicVillaAPI2/Models/VillaModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVillaAPI2.Models
+{
+    public class VillaModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
